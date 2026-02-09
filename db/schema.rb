e136bcca5f5d9de5959e6e60afbeaf51286bf88d
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2026-02-09 13:42:04 +0000

    Run migrations for TrainingTemplate, PlannedExercise, PlannedSet and Exercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_02_165203) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_08_125054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "exercises", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "exercise_type"
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_exercises_on_name", unique: true
+  end
+
+  create_table "planned_exercises", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "exercise_id", null: false
+    t.bigint "training_template_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["exercise_id"], name: "index_planned_exercises_on_exercise_id"
+    t.index ["training_template_id"], name: "index_planned_exercises_on_training_template_id"
+  end
+
+  create_table "planned_sets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "duration_seconds"
+    t.bigint "planned_exercise_id", null: false
+    t.integer "reps"
+    t.integer "set_number"
+    t.datetime "updated_at", null: false
+    t.float "weight"
+    t.index ["planned_exercise_id"], name: "index_planned_sets_on_planned_exercise_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +51,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_165203) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
+  create_table "training_templates", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["name"], name: "index_training_templates_on_name", unique: true
+    t.index ["user_id"], name: "index_training_templates_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
@@ -31,5 +68,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_165203) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  add_foreign_key "planned_exercises", "exercises"
+  add_foreign_key "planned_exercises", "training_templates"
+  add_foreign_key "planned_sets", "planned_exercises"
   add_foreign_key "sessions", "users"
+  add_foreign_key "training_templates", "users"
 end
